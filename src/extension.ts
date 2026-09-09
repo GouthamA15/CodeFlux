b8f69b22b33909621373597fc7c79dcf0e1738d3
GouthamA15 <lucky.masna1@gmail.com>
2026-09-09 22:14:55 +0530

    Init

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('codeflux.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeFlux!');
+		vscode.window.showInformationMessage('Hello World from CodeFlux! how you doing');
 	});
 
 	context.subscriptions.push(disposable);
